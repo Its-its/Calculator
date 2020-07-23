6471570c77b7c43174d63361d1679188eb6f7ceb
Timothy <2779546+Its-its@users.noreply.github.com>
2020-07-23 06:48:19 +0000

    Updated Tokenizer

--- parser/src/tokenizer.rs
+++ parser/src/tokenizer.rs
@@ -40,16 +40,14 @@ pub struct Tokenizer<'a> {
 	pub factory: &'a Factory,
 	compiled: Vec<ExprToken>,
 
-	value: &'a str,
-	pos: usize
+	value: StringSlice<'a>
 }
 
 impl<'a> Tokenizer<'a> {
 	pub fn new(value: &'a str, factory: &'a Factory) -> Self {
 		Tokenizer {
 			factory,
-			value,
-			pos: 0,
+			value: StringSlice::new(value),
 			compiled: Vec::new(),
 		}
 	}
@@ -65,7 +63,7 @@ impl<'a> Tokenizer<'a> {
 	pub fn parse(&mut self) -> Result<()> {
 		let mut compiled = Vec::new();
 
-		while !self.is_finished() {
+		while !self.value.is_finished() {
 			let found = None
 				.or_else(|| self.remove_non_essiential())
 				.or_else(|| self.parse_tokens(&DOUBLE_CHAR_TOKENS))
@@ -76,7 +74,7 @@ impl<'a> Tokenizer<'a> {
 			if let Some(found) = found {
 				compiled.push(found);
 			} else {
-				print_dbg!("Unable to finish. Stopped at: {:?}", self.value.get(self.pos..).unwrap_or(""));
+				print_dbg!("Unable to finish. Stopped at: {:?}", self.value.get_remaining_str());
 
 				break;
 			}
@@ -89,7 +87,7 @@ impl<'a> Tokenizer<'a> {
 
 
 	fn remove_non_essiential(&mut self) -> ParseResult {
-		if self.consume_if(" ") {
+		if self.value.consume_if(" ") {
 			Some(ExprToken::Whitespace)
 		} else {
 			None
@@ -97,7 +95,7 @@ impl<'a> Tokenizer<'a> {
 	}
 
 	fn parse_number(&mut self) -> ParseResult {
-		let remains = self.get_remaining_str();
+		let remains = self.value.get_remaining_str();
 
 		let builder = Regex::new(r#"^((?:[0-9,]+)?\.?(?:e-?)?(?:[0-9]+)?)"#).unwrap();
 
@@ -116,7 +114,7 @@ impl<'a> Tokenizer<'a> {
 					number.replace(",", "").parse().unwrap()
 				};
 
-				self.consume_amount(end);
+				self.value.consume_amount(end);
 
 				Some(ExprToken::Number(num))
 			} else {
@@ -128,21 +126,23 @@ impl<'a> Tokenizer<'a> {
 	}
 
 	fn parse_literal(&mut self) -> ParseResult {
-		let remains = self.get_remaining_str();
+		let remains = self.value.get_remaining_str();
 
 		let builder = Regex::new(r#"^([^\d\s\(\)\[\]\{\}]+)"#).unwrap();
 
 		if let Some(found) = builder.find(remains) {
-			if found.end() != 0 {
-				let value = remains.get(0..found.end()).unwrap().to_owned();
+			let end = found.end();
 
-				self.consume_amount(found.end());
+			if end != 0 {
+				let found = found.as_str().to_string();
+
+				self.value.consume_amount(end);
 
 				// Check if it's a const.
-				if let Some(item) = self.factory.find_const(value.as_str()) {
+				if let Some(item) = self.factory.find_const(found.as_str()) {
 					Some(ExprToken::Number(item))
 				} else {
-					Some(ExprToken::Literal(value))
+					Some(ExprToken::Literal(found))
 				}
 			} else {
 				None
@@ -154,42 +154,13 @@ impl<'a> Tokenizer<'a> {
 
 	fn parse_tokens(&mut self, tokens: &[Id<ExprToken>]) -> ParseResult {
 		for token in tokens {
-			if self.consume_if(token.0) {
+			if self.value.consume_if(token.0) {
 				return Some(token.1.clone());
 			}
 		}
 
 		None
 	}
-
-	//
-
-	fn get_remaining_str(&self) -> &str {
-		self.value.get(self.pos..)
-		.unwrap_or("")
-	}
-
-	fn consume_if(&mut self, next_token: &str) -> bool {
-		if let Some(grabbed) = self.value.get(self.pos..self.pos + next_token.len()) {
-			if grabbed == next_token {
-				self.consume_amount(next_token.len());
-				true
-			} else {
-				false
-			}
-		} else {
-			false
-		}
-	}
-
-	fn consume_amount(&mut self, value: usize) {
-		self.pos += value;
-	}
-
-	pub fn is_finished(&self) -> bool {
-		self.value.len() <= self.pos
-	}
-
 
 	pub fn find_tokens<'b>(&'b self, tokens: &'b [TokenType]) -> impl Iterator<Item = &[ExprToken]> + 'b {
 		self.find_tokens_index(tokens)
@@ -238,4 +209,51 @@ struct Found {
 	end: usize, // Finished pos.
 	step: usize, // Current token step.
 	pass: bool // Should pass filter.
+}
+
+
+struct StringSlice<'a> {
+	value: &'a str,
+	pos: usize
+}
+
+
+impl<'a> StringSlice<'a> {
+	pub fn new(value: &'a str) -> StringSlice<'a> {
+		StringSlice {
+			value,
+			pos: 0
+		}
+	}
+
+	fn consume_if(&mut self, next_token: &str) -> bool {
+		if let Some(grabbed) = self.get_until(next_token.len()) {
+			let is_correct = grabbed == next_token;
+
+			if is_correct {
+				self.consume_amount(next_token.len());
+			}
+
+			is_correct
+		} else {
+			false
+		}
+	}
+
+	pub fn consume_amount(&mut self, value: usize) {
+		self.pos += value;
+	}
+
+	pub fn is_finished(&self) -> bool {
+		self.value.len() <= self.pos
+	}
+
+	pub fn get_remaining_str(&self) -> &str {
+		self.value.get(self.pos..)
+		.unwrap_or("")
+	}
+
+	pub fn get_until(&self, value: usize) -> Option<&str> {
+		self.value.get(self.pos..self.pos + value)
+	}
 }
